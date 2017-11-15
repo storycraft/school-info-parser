0d235188874d5f8a13a0239e9fff8952f6aa9dd0
StoryCraft <dbsxoal123@naver.com>
2017-11-15 20:55:27 +0900

    v1.1 update

--- src/school_info_parser.js
+++ src/school_info_parser.js
@@ -1,6 +1,3 @@
-//async await support
-import babelPolyfill from 'babel-polyfill';
-
 import SchoolMenuParser from './school_menu_parser.js';
 import SchoolScheduleParser from './school_schedule_parser.js';
 import SchoolType from './school_type.js';
