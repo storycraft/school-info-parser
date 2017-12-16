0ac3fa8b9779959bfbfc93030cfcadedd1f07cda
StoryCraft <dbsxoal123@naver.com>
2017-12-16 15:53:37 +0900

    remove build files deprecated support

--- src/SchoolInfoParser.js
+++ src/SchoolInfoParser.js
@@ -11,4 +11,12 @@ import ScheduleParser from './parser/ScheduleParser.js';
 import SchoolType from './SchoolType.js';
 import SchoolLocation from './SchoolLocation.js';
 
-export { SchoolParser, MenuParser, ScheduleParser, SchoolType, SchoolLocation, /*deprecated*/ MenuParser as SchoolMenuParser,/*deprecated*/ ScheduleParser as SchoolScheduleParser };
+function deprecatedScheduleParser(schoolLocation,schoolCode,schoolType){
+  return new ScheduleParser(schoolLocation,schoolCode,schoolType);
+}
+
+function deprecatedMenuParser(schoolLocation,schoolCode,schoolType){
+  return new MenuParser(schoolLocation,schoolCode,schoolType);
+}
+
+export { SchoolParser, MenuParser, ScheduleParser, SchoolType, SchoolLocation, /*deprecated*/ deprecatedMenuParser as SchoolMenuParser,/*deprecated*/ deprecatedScheduleParser as SchoolScheduleParser };
